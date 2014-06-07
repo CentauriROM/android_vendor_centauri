bc48ac16d74e80fb51af0bbfa73b86c57dce2b53
Tony Malagisi <mtb3000gt@gmail.com>
2014-06-07 14:50:15 -0400

    Fix Typo

--- config/nfc_enhanced.mk
+++ config/nfc_enhanced.mk
@@ -1,3 +1,3 @@
 # device supports nfc enhanced
 PRODUCT_COPY_FILES += \
-    vendor/config/permissions/com.centauri.nfc.enhanced.xml:system/etc/permissions/com.centauri.nfc.enhanced.xml
+    vendor/centauri/config/permissions/com.centauri.nfc.enhanced.xml:system/etc/permissions/com.centauri.nfc.enhanced.xml
